dc3b6650b1f322c41931c8ce333bd959bb802729
iankranz <ian@iankranz.com>
2023-05-25 00:03:32 -0500

    add placeholder page

---
+++ app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg">
+    <h1 class="title">Need a dev?</h1>
+    <p class="description">Try <a href="#" class="link">needa.dev</a></p>
+    <p class="description">
+      email
+      <a href="mailto:hello@needa.dev" class="link">hello@needa.dev</a> for more
+      info
+    </p>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0rem;
+  padding: 0rem;
+  font-family: "Comfortaa", sans-serif;
+}
+.bg {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #333333;
+  height: 100vh;
+  width: 100%;
+  margin: 0rem;
+  padding: 0rem;
+}
+.title {
+  padding: 4rem 0rem 0rem 0rem;
+  color: #ffffff;
+}
+.description {
+  color: #ffffff;
+}
+
+.link {
+  color: #00aaaa;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Comfortaa: true,
+    },
+  },
+});
