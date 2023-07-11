273297b6996a58c5d8915a0bd5dc8869fefeb044
iankranz <ian@iankranz.com>
2023-07-11 00:57:38 -0400

    remove tailwind for deploy test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/google-fonts", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/google-fonts"],
   googleFonts: {
     families: {
       Comfortaa: true
